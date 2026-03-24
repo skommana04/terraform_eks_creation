213571186f2e048d5d7f70d0b43cec72998fed82
skommana04 <skommana04@gmail.com>
2026-03-24 09:40:46 -0500

    a

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "vpc_id" {
     name = "/${var.project}/${var.environment}/vpc_id"
-    type = "string"
+    type = "String"
     value = module.vpc.vpc_id
 }
--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -3,7 +3,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       #version = "~> 6.0"
-      version = "6.16.0"
+      #version = "6.16.0"
     }
   }
 
