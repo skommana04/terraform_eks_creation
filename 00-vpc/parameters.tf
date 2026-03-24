648f3450313962a29b9d8da02a3e488df2838354
skommana04 <skommana04@gmail.com>
2026-03-24 12:09:15 -0500

    a

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -3,3 +3,19 @@ resource "aws_ssm_parameter" "vpc_id" {
     type = "String"
     value = module.vpc.vpc_id
 }
+/*
+In Terraform, module.vpc.public_subnet_ids is a list. AWS Systems Manager (SSM) expects a StringList to be a series of values separated by commas, all contained within one string.
+
+The join function is the "bridge" that fixes this:
+
+Input: ["subnet-123", "subnet-456"] (List)
+
+Action: join(",", ...)
+
+Output: "subnet-123,subnet-456" (String)
+*/
+resource "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project}/${var.environment}/public_subnet_ids"
+    type = "StringList"
+    value = join(",",module.vpc.public_subnet_ids)
+}
