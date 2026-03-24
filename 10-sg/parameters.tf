2cb5eb6efb3ff93449419dad10da601d1701115b
skommana04 <skommana04@gmail.com>
2026-03-24 12:39:20 -0500

    a

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -1,7 +1,9 @@
 resource "aws_ssm_parameter" "sg_ids" {
     count = length(var.sg_names)
-    name = "/${var.project}/${var.environment}/${var.component}_sg_id"
+    name = "/${var.project}/${var.environment}/${var.sg_names[count.index]}_sg_id"
     type = "String"
     value = module.sg[count.index].sg_id
     overwrite = true 
 }
+
+
